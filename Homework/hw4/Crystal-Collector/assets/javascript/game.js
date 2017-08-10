6995bad72000430bb7ce3278ada372a803cf2af0
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-10 17:19:20 -0500

    initial build

--- Homework/hw4/Crystal-Collector/assets/javascript/game.js
+++ Homework/hw4/Crystal-Collector/assets/javascript/game.js
@@ -1,37 +1,140 @@
-var numWins = 0, numLosses = 0;
-$("#numWins").text(numWins);
-$("#numLosses").text(numLosses);
+/****************************************************************************
+ ****************************************************************************
+    
+    Create an object of Crystal Collector game
+    
+*****************************************************************************
+*****************************************************************************/
+var game;
 
-var targetSum = 50;
-var currentSum = 0;
+var CrystalCollectorGame = function() {
+    /************************************************************************
+        
+        Private variables
+        
+    *************************************************************************/
+    // Variables for the game
+    var numWins = 0, numLosses = 0;
 
-$(document).ready(function() {
-    $("#targetSum").text(targetSum);
-    $("#currentSum").text(currentSum);
+    // Variables for the user
+    var targetSum, currentSum;
+    var numCrystals = 4, crystalValues = new Array(numCrystals);
+    
 
-    var powers = new Array(4);
+    /************************************************************************
+        
+        Start a new game
+        
+    *************************************************************************/
+    this.startNewGame = function() {
+        // Choose a random sum between 19 and 120
+        targetSum = Math.floor(102 * Math.random()) + 19;
 
-    for (var i = 0; i < powers.length; i++) {
-        powers[i] = Math.floor(10 * Math.random()) + 1;
+        // Reset current sum
+        currentSum = 0;
+
+        // Assign a value between 1 and 12 to each crystal
+        for (var i = 0; i < numCrystals; i++) {
+            crystalValues[i] = Math.floor(12 * Math.random()) + 1;
+        }
+
+        // Display messages
+        this.displayNumWins();
+        this.displayNumLosses();
+        this.displayTargetSum();
+        this.displayCurrentSum();
     }
 
-    $.each(powers, function(index, value) {
-        console.log("Crystal " + (index + 1) + ": " + value);
+    
+    /************************************************************************
+        
+        Display methods
+        
+    *************************************************************************/
+    this.displayNumWins = function() {
+        $("#numWins").text(numWins);
+    }
+
+    this.displayNumLosses = function() {
+        $("#numLosses").text(numLosses);        
+    }
+
+    this.displayTargetSum = function() {
+        $("#targetSum").text(targetSum);
+    }
+
+    this.displayCurrentSum = function() {
+        $("#currentSum").text(currentSum);
+    }
+
+
+    /************************************************************************
+        
+        Get methods
+        
+    *************************************************************************/
+    this.getCrystalValues = function() {
+        return crystalValues;
+    }
+
+
+    /************************************************************************
+        
+        Set (update) methods
+        
+    *************************************************************************/
+    this.updateCurrentSum = function(changeBy) {
+        currentSum += changeBy;
+    }
+
+
+    /************************************************************************
+        
+        Query methods
+        
+    *************************************************************************/
+    this.isCurrentSumOver = function() {
+        return (currentSum > targetSum);
+    }
+}
+
+
+
+/****************************************************************************
+ ****************************************************************************
+    
+    Start a new game when the page loads for the first time
+    
+*****************************************************************************
+*****************************************************************************/
+$(document).ready(function() {
+    game = new CrystalCollectorGame();
+
+    game.startNewGame();
+
+    
+    /************************************************************************
+        
+        Respond to user's actions
+        
+    *************************************************************************/
+    $.each(game.getCrystalValues(), function(index, value) {
+//        console.log("Crystal " + (index + 1) + ": " + value);
 
         var elementTag = ".crystal:nth-child(" + (index + 1) + ")";
 
         $(elementTag).on("click", function() {
-            currentSum += value;
-            $("#currentSum").text(currentSum);
+            game.updateCurrentSum(value);
+
+            game.displayCurrentSum();
+
+            if (game.isCurrentSumOver()) {
+//                console.log("You lost.");
+
+            } else {
+//                console.log("Keep going!");
+
+            }
         });
     });
-
-
-    if (currentSum === targetSum) {
-        console.log("You won.");
-
-    } else if (currentSum > targetSum) {
-        console.log("You lost.");
-
-    }
 });
