4d06c7239762cf711d00924d2899a78270d85152
ijlee2 <ijlee2@users.noreply.github.com>
2017-08-18 16:07:17 -0500

    day 15 exercises

--- exercises/day15/nyt-api/app.js
+++ exercises/day15/nyt-api/app.js
@@ -1,19 +1,29 @@
 var api_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-$(document).ready(function(){
-    $("#searchMain").on("click",function(){
-        var query=$("#query").val();
-        var yearEnd=$("#yearEnd").val();
-        var yearBegin=$("#yearBegin").val();
-        var numPagesReturned=$("#numPagesReturned").find(":selected").text();
-   
-        api_url += '?' + $.param({"api-key"   : "97cf7b2b109345febdef229d0e7d8edc",
-                                  "q"         : query,
-                                  "begin_date": yearBegin,
-                                  "end_date"  : yearEnd,
-                                  "sort"      : "newest"
-        });
+$(document).ready(function() {
+    $("#button_search").on("click", function() {
+        var query            = $("#query").val();
+        var yearBegin        = $("#yearBegin").val() + "0101";
+        var yearEnd          = $("#yearEnd").val()   + "1231";
+        var numPagesReturned = parseInt($("#numPagesReturned").find(":selected").text());
 
+        var parameters = {"api-key"   : "97cf7b2b109345febdef229d0e7d8edc",
+                          "q"         : query,
+                          "begin_date": yearBegin,
+                          "end_date"  : yearEnd,
+                          "sort"      : "newest"};
+
+        if (yearBegin === "0101") {
+            delete parameters.begin_date;
+        }
+
+        if (yearEnd === "1231") {
+            delete parameters.end_date;
+        }
+        
+        api_url += '?' + $.param(parameters);
+
+        // Call the API
         $.ajax({
             "url"   : api_url,
             "method": "GET"
@@ -31,7 +41,6 @@ $(document).ready(function(){
                            </div>`;
             }
 
-
             $("#results").html(output);
 
             $(".pageReturned").on("click", function() {
@@ -42,11 +51,5 @@ $(document).ready(function(){
             throw error;
 
         });
-
-
-
-            });
-        });
-            // Add parameters
-
-
+    });
+});
