49d4673481f935e2938d2143763a5824d4606eee
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-01 10:18:44 -0500

    day 25 exercises

--- exercises/day25/sort_numbers.js
+++ exercises/day25/sort_numbers.js
@@ -1,5 +1,5 @@
 const numbers = process.argv.slice(2);
 
-const numbersSorted = numbers.sort((a, b) => a - b);
+const numbersSorted = numbers.sort((a, b) => parseFloat(a) - parseFloat(b));
 
 console.log(numbersSorted);
