a92b1285abecb6163862bd8223f093f1270cdcf2
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-01 13:34:36 -0500

    day 25 exercises

--- exercises/day25/user_prompt.js
+++ exercises/day25/user_prompt.js
@@ -12,5 +12,53 @@
 // See the inquirer GitHub documentation "examples" page if you need help.
 
 // Remember to be creative!
-
 // ========================================================================
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        "type"   : "input",
+        "name"   : "name",
+        "message": "What is your name?"
+    },
+    {
+        "type"   : "confirm",
+        "name"   : "confirm",
+        "message": "Is this you?",
+        "default": true
+    },
+    {
+        "type"   : "password",
+        "name"   : "password",
+        "message": "What is your password?"
+    },
+    {
+        "type"   : "list",
+        "name"   : "bloodType",
+        "message": "Select your blood type.",
+        "choices": ["A", "B", "O", "AB"]
+    },
+    {
+        "type"   : "checkbox",
+        "name"   : "hobbies",
+        "message": "Select your hobbies.",
+        "choices": ["Eating", "Drinking", "Sleeping", "Studying"]
+    },
+    {
+        "type"   : "password",
+        "name"   : "password_check",
+        "message": "Type in your password again."
+    }
+
+]).then((response) => {
+    if (response.confirm && response.password_check === response.password) {
+        console.log(`\nWelcome ${response.name}.`);
+        console.log(`\nYour blood type is ${response.bloodType}.`);
+        console.log(`\nYour hobbies are ${response.hobbies.join(", ")}.`);
+
+    } else {
+        console.log("Please try again!");
+
+    }
+
+});
