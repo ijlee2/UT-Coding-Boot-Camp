e7e311a4aae48dd1b25a888be5e4829d8cff2ade
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-01 18:02:52 -0500

    initial build

--- homework/hw7/rps-multiplayer/assets/javascript/app2.js
+++ homework/hw7/rps-multiplayer/assets/javascript/app2.js
@@ -6,93 +6,52 @@
 *****************************************************************************
 *****************************************************************************/
 const config = {
-    apiKey           : "AIzaSyDXvqGhdA0Ub2OVaR2uAYRw4qMWxAJXLSg",
-    authDomain       : "rps-multiplayer-53644.firebaseapp.com",
-    databaseURL      : "https://rps-multiplayer-53644.firebaseio.com",
-    projectId        : "rps-multiplayer-53644",
-    storageBucket    : "rps-multiplayer-53644.appspot.com",
-    messagingSenderId: "931155377111"
+    "apiKey"           : "AIzaSyDXvqGhdA0Ub2OVaR2uAYRw4qMWxAJXLSg",
+    "authDomain"       : "rps-multiplayer-53644.firebaseapp.com",
+    "databaseURL"      : "https://rps-multiplayer-53644.firebaseio.com",
+    "projectId"        : "rps-multiplayer-53644",
+    "storageBucket"    : "rps-multiplayer-53644.appspot.com",
+    "messagingSenderId": "931155377111"
 };
 
 firebase.initializeApp(config);
 
-const database   = firebase.database();
-const playersRef = database.ref("players");
+const database_players = firebase.database().ref("players");
 
 
 
 /****************************************************************************
  ****************************************************************************
     
-    Rock, Paper, Scissors Game
+    Initialize
     
 *****************************************************************************
 *****************************************************************************/
 // Global variables
 const numPlayersAllowed = 2;
-let   numPlayersInMatch;
-let   players = [], availableID = 0;
-let   myID, arrayID, turn;
-let   messages;
+let players, numPlayers, myID;
+let turn;
+let messages;
 
-
-/****************************************************************************
+// Find out who are playing the game
+database_players.on("value", (snapshot) => {
+    players = snapshot.val();
     
-    Set the database behavior
-    
-*****************************************************************************/
-function loadDatabase() {
-    playersRef.on("child_added", function(snapshot) {
-        // Get the player
-        const player = snapshot.val();
-
-        // Update the array
-        players.push(player);
+    if (players) {
+        numPlayers = players.filter(p => p !== -1).length;
         
-        database.ref("numPlayersInMatch").set(numPlayersInMatch + 1);
+        console.log("Number of spots taken: " + numPlayers);
+        console.log(players);
         
-        refreshDisplay();
-    });
-
-    playersRef.on("child_removed", function(snapshot) {
-        // Get the player
-        const player = snapshot.val();
-        myID = player.id;
-
-        // Update the array
-        findArrayID();
-        players.splice(arrayID, 1);
-        
-        database.ref("numPlayersInMatch").set(numPlayersInMatch - 1);
-        
-        refreshDisplay();
-    });
-
-    database.ref("numPlayersInMatch").on("value", function(snapshot) {
-        numPlayersInMatch = (snapshot.val()) ? snapshot.val() : 0;
-    });
-
-    database.ref("turn").on("value", function(snapshot) {
-        turn = (snapshot.val()) ? snapshot.val() : undefined;
-    });
-}
-
-
-/****************************************************************************
-    
-    Query functions
-    
-*****************************************************************************/
-function findArrayID() {
-    // Find the player in the array
-    for (arrayID = 0; arrayID < players.length; arrayID++) {
-        if (players[arrayID].id === myID) {
-//            myPlayer = players[arrayID];
-
-            break;
+    // Initialize the database
+    } else {
+        for (let i = 0; i < numPlayersAllowed; i++) {
+            database_players.child(i).set(-1);
         }
+
     }
-}
+});
+
 
 
 /****************************************************************************
@@ -128,24 +87,27 @@ function refreshDisplay() {
 
 /****************************************************************************
     
-    Add or delete a player
+    Add a player
     
 *****************************************************************************/
 function addPlayer() {
-    // Add the player
-    if (numPlayersInMatch < numPlayersAllowed) {
-        myID = numPlayersInMatch;
-
-        const player = {"id"       : myID,
-                        "name"     : $("#input_name").val().trim(),
+    if (numPlayers < numPlayersAllowed) {
+        const player = {"name"     : $("#input_name").val().trim(),
                         "choice"   : -1,
                         "numWins"  : 0,
-                        "numLosses": 0};    
+                        "numLosses": 0};
 
-        const playerRef = playersRef.child(myID);
+        // Add the player to the next available spot
+        for (let i = 0; i < numPlayersAllowed; i++) {
+            if (players[i] === -1) {
+                myID = i;
 
-        playerRef.set(player);
-        playerRef.onDisconnect().remove();
+                database_players.child(i).set(player);
+                database_players.child(i).onDisconnect().set(-1);
+
+                break;
+            }
+        }
 
         displayPage(1);
     }
@@ -158,8 +120,6 @@ function addPlayer() {
     
 *****************************************************************************/
 $(document).ready(function() {
-    loadDatabase();
-
     displayPage(0);
 
     $("#input_name").on("keyup", function(e) {
