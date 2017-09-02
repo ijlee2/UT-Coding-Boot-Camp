2ab065924d98868e2154fbf630eb413d21bd9cc2
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-02 07:37:29 -0500

    final build

---
+++ homework/hw8/keys_template.js
@@ -0,0 +1,15 @@
+exports.twitter = {
+    "consumer_key"       : "---enter yours here---",
+    "consumer_secret"    : "---enter yours here---",
+    "access_token_key"   : "---enter yours here---",
+    "access_token_secret": "---enter yours here---",
+}
+
+exports.spotify = {
+    "id"    : "---enter yours here---",
+    "secret": "---enter yours here---"
+}
+
+exports.omdb = {
+    "key": "---enter yours here---"
+}
