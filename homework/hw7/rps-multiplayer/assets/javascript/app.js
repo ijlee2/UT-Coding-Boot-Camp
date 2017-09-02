32e40587fddf670652aefa27bf1e69a270c2adfb
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-01 20:51:41 -0500

    initial build

--- homework/hw7/rps-multiplayer/assets/javascript/app.js
+++ homework/hw7/rps-multiplayer/assets/javascript/app.js
@@ -42,6 +42,8 @@ database_players.on("value", (snapshot) => {
     
     if (players) {
         numPlayers = players.filter(p => p !== -1).length;
+
+        database_turn.set((numPlayers === numPlayersAllowed) ? 0 : null);
         
     // Initialize the database
     } else {
@@ -58,78 +60,19 @@ database_players.on("value", (snapshot) => {
 });
 
 // Find out whose turn it is
-database_turn.on("value", (snapshot) => turn = (snapshot.val()) ? snapshot.val() : 0);
+database_turn.on("value", (snapshot) => {turn = snapshot.val(); console.log("It is Player " + turn + "'s now.");});
 
 // Display chat messages
-database_messages.on("child_added", (snapshot) => {
-    $("#messages").append(snapshot.val());
-});
+database_messages.on("child_added", (snapshot) => $("#messages").append(snapshot.val()));
+
 
 
 /****************************************************************************
+ ****************************************************************************
     
     Game mechanics
     
-*****************************************************************************/
-function addPlayer(name) {
-    if (numPlayers < numPlayersAllowed) {
-        const player = {
-            "name"     : name,
-            "choice"   : -1,
-            "numWins"  : 0,
-            "numLosses": 0
-        };
-
-        // Add the player to the next available spot
-        for (let i = 0; i < numPlayersAllowed; i++) {
-            if (players[i] === -1) {
-                myID = i;
-
-                database_players.child(i).set(player);
-                database_players.child(i).onDisconnect().set(-1);
-
-                break;
-            }
-        }
-
-        displayPage(1);
-    }
-}
-
-
-/****************************************************************************
-    
-    Display functions
-    
-*****************************************************************************/
-function displayPage(page) {
-    $(".page").css({"display": "none"});
-    $(`.page:nth-of-type(${page + 1})`).css({"display": "block"});
-}
-
-function refreshDisplay() {
-    let element;
-
-    for (let i = 0; i < numPlayersAllowed; i++) {
-        element = `#player${i} > `;
-
-        if (players[i] !== -1) {
-            $(`${element}.name`).html(`<h2>${players[i].name}</h2>`);
-            $(`${element}.display`).html("<p>Welcome to RPS Multiplayer!</p>");
-
-        } else {
-            $(`${element}.name`).html(`<h2>Player ${i + 1}</h2>`);
-            $(`${element}.display`).html("<p>Waiting for the player to join...<p>");
-
-        }
-    }
-}
-
-
-/****************************************************************************
-    
-    Wait for user actions
-    
+*****************************************************************************
 *****************************************************************************/
 $(document).ready(function() {
     displayPage(0);
@@ -155,4 +98,74 @@ $(document).ready(function() {
 // Name can consist of letters and numbers only
 function checkName(name) {
     return name.match(/^[a-z0-9]+$/i);
+}
+
+function addPlayer(name) {
+    if (numPlayers < numPlayersAllowed) {
+        const player = {
+            "name"     : name,
+            "choice"   : -1,
+            "numWins"  : 0,
+            "numLosses": 0
+        };
+
+        // Add the player to the next available spot
+        for (let i = 0; i < numPlayersAllowed; i++) {
+            if (players[i] === -1) {
+                myID = i;
+
+                database_players.child(myID).set(player);
+                database_players.child(myID).onDisconnect().set(-1);
+
+                break;
+            }
+        }
+
+        displayPage(1);
+    }
+}
+
+
+
+/****************************************************************************
+ ****************************************************************************
+    
+    Display methods
+    
+*****************************************************************************
+*****************************************************************************/
+function displayPage(page) {
+    $(".page").css({"display": "none"});
+    $(`.page:nth-of-type(${page + 1})`).css({"display": "block"});
+}
+
+function refreshDisplay() {
+    let element;
+    
+    if (turn === null) {
+        for (let i = 0; i < numPlayersAllowed; i++) {
+            element = `#player${i} > `;
+
+            if (players[i] !== -1) {
+                $(`${element}.name`).html(`<h2>${players[i].name}</h2>`);
+
+            } else {
+                $(`${element}.name`).html(`<h2>Waiting for Player ${i + 1}</h2>`);
+
+            }
+        }
+
+    } else {
+        for (let i = 0; i < numPlayersAllowed; i++) {
+            $(`#player${i} > .name`).html(`<h2>${players[i].name}</h2>`);
+        }
+
+        if (turn === myID) {
+            $(`#player${myID} > .display`).html("<div>Rock</div><div>Paper</div><div>Scissors</div>");
+
+        } else {
+            $(`#player${myID} > .display`).html(`<p>Waiting for ${players[turn].name} to make a move.<p>`);
+
+        }
+    }
 }
