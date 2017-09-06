5be77305ce064b12b07dd884d640a62227d91df7
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-06 10:30:39 -0500

    day 28 exercises

--- exercises/day28/raining_cats_and_dogs.js
+++ exercises/day28/raining_cats_and_dogs.js
@@ -1,21 +1,19 @@
+function makeNoise() {
+    if (this.raining) {
+        console.log(this.noise);
+    }
+}
+
 const dogs = {
     "raining"  : true,
     "noise"    : "Woof!",
-    "makeNoise": function() {
-        if (this.raining) {
-            console.log(this.noise);
-        }
-    }
+    "makeNoise": makeNoise
 }
 
 const cats = {
     "raining"  : false,
     "noise"    : "Meow!",
-    "makeNoise": function() {
-        if (this.raining) {
-            console.log(this.noise);
-        }
-    }
+    "makeNoise": makeNoise
 }
 
 function massHysteria(dogs, cats) {
