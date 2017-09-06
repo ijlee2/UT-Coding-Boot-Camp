ae6c2ad0cedd1889f7706db3aa76debe840d520c
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-06 13:54:25 -0500

    day 28 exercises

--- exercises/day28/programmers_with_inquirer.js
+++ exercises/day28/programmers_with_inquirer.js
@@ -1,9 +1,14 @@
+const inquirer = require("inquirer");
+
+const programmers = [];
+
 function Programmer(parameters) {
     for (let key in parameters) {
         this[key] = parameters[key];
     }
 
     this.displayInformation = function() {
+        console.log();
         console.log(`Name     : ${this.name}`);
         console.log(`Position : ${this.position}`);
         console.log(`Age      : ${this.age}`);
@@ -12,37 +17,54 @@ function Programmer(parameters) {
     }
 }
 
-const inquirer = require("inquirer");
+function enterProgrammer() {
+    inquirer.prompt([
+        {
+            "type"   : "input",
+            "name"   : "name",
+            "message": "What is your name?"
+        },
+        {
+            "type"   : "input",
+            "name"   : "position",
+            "message": "What is your current position?"
+        },
+        {
+            "type"   : "input",
+            "name"   : "age",
+            "message": "How old are you?"
+        },
+        {
+            "type"   : "input",
+            "name"   : "language",
+            "message": "What is your favorite programming language?"
+        },
+        {
+            "type"   : "confirm",
+            "name"   : "continue",
+            "message": "Enter another programmer?",
+            "default": true
+        }
 
-inquirer.prompt([
-    {
-        "type"   : "input",
-        "name"   : "name",
-        "message": "What is your name?"
-    },
-    {
-        "type"   : "input",
-        "name"   : "position",
-        "message": "What is your current position?"
-    },
-    {
-        "type"   : "input",
-        "name"   : "age",
-        "message": "How old are you?"
-    },
-    {
-        "type"   : "input",
-        "name"   : "language",
-        "message": "What is your favorite programming language?"
-    }
+    ]).then(response => {
+        const programmer = new Programmer({
+            "name"    : response.name,
+            "position": response.position,
+            "age"     : parseInt(response.age),
+            "language": response.language
+        });
 
-]).then(response => {
-    const programmer = new Programmer({
-        "name"    : response.name,
-        "position": response.position,
-        "age"     : parseInt(response.age),
-        "language": response.language
+        programmers.push(programmer);
+
+        if (response.continue) {
+            console.log();
+            enterProgrammer();
+
+        } else {
+            programmers.forEach(p => p.displayInformation());
+            
+        }
     });
+}
 
-    programmer.displayInformation();
-});
+enterProgrammer();
