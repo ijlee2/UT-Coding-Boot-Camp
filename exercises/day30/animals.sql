8f0540b6615ca4f0da6ced167e17c258eb07c8b9
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-08 14:05:30 -0500

    day 30 exercises

--- exercises/day30/animals.sql
+++ exercises/day30/animals.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS animals_db;
 CREATE DATABASE animals_db;
 USE animals_db;
 
@@ -9,3 +10,20 @@ CREATE TABLE people(
     pet_age  INTEGER(10),
     PRIMARY KEY (id)
 );
+
+-- Create new rows --
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ("Ahmed", true, "Rockington", 100);
+
+INSERT INTO people (name, has_pet, pet_name, pet_age)
+VALUES ("Jacob", true, "Misty", 10);
+
+INSERT INTO people (name, has_pet)
+VALUES ("Peter", true);
+
+-- Update a row --
+UPDATE people
+SET has_pet = true, pet_name = "Franklin", pet_age = 2
+WHERE id = 3;
+
+SELECT * FROM people;
--- exercises/day30/favorite_db.sql
+++ exercises/day30/favorite_db.sql
@@ -8,23 +8,19 @@ USE favorite_db;
 
 -- Creates the table "favorite_foods" within favorite_db --
 CREATE TABLE favorite_foods (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
     -- Make a string column called "food" which cannot contain null --
-    food VARCHAR(30) NOT NULL,
+    food VARCHAR(50) NOT NULL,
     -- Make an numeric column called "score" --
-    score INTEGER(10),
-    PRIMARY KEY (id)
+    score INTEGER(10)
 );
 
 CREATE TABLE favorite_songs (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
     -- Make a string column called "song" which cannot contain null --
     song VARCHAR(50) NOT NULL,    
     -- Make a string column called "artist" --
     artist VARCHAR(50),
     -- Make an integer column called "score" --
-    score INTEGER(10),
-    PRIMARY KEY (id)
+    score INTEGER(10)
 );
 
 CREATE TABLE favorite_movies (
@@ -39,3 +35,22 @@ CREATE TABLE favorite_movies (
     -- Set the primary key of the table to id --
     PRIMARY KEY (id)
 );
+
+INSERT INTO favorite_foods VALUES
+    ("kiwi", 8),
+    ("orange", 10),
+    ("banana", 7);
+
+INSERT INTO favorite_songs VALUES
+    ("Look Alive Sunshine", "My Chemical Romance", 9),
+    ("Feet Don't Fail Me", "Queens of the Stone Age", 10),
+    ("Hourglass", "S U R V I V E", 9);
+    
+INSERT INTO favorite_movies VALUES
+    (1, "The Bourne Ultimatum", true, 9),
+    (2, "Charlie and the Chocolate Factory", false, 3),
+    (3, "Inception", true, 8);
+
+SELECT * FROM favorite_foods;
+SELECT * FROM favorite_songs;
+SELECT * FROM favorite_movies;
