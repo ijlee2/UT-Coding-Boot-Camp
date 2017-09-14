8cd81f8692cb877e983c0d7d9bf21c43ebb9953c
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-14 11:01:51 -0500

    day 34 exercises

--- exercises/day34/server6.js
+++ exercises/day34/server6.js
@@ -1,16 +1,22 @@
 // Dependencies
+// =============================================================
 const express    = require("express");
 const bodyParser = require("body-parser");
+const path       = require("path");
 
+// Sets up the Express App
+// =============================================================
 const app  = express();
 const PORT = 3000;
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({"extended": true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({"type": "application/vnd.api+json"}));
 
-// Data
+// Star Wars Characters (DATA)
+// =============================================================
 const characters = [
     {
         "routeName"  : "yoda",
@@ -34,10 +40,19 @@ const characters = [
 ];
 
 // Routes
+// =============================================================
+// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.send("Welcome to the Star Wars Page!");
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "view.html"));
 });
 
+// Get all characters
+app.get("/all", function(req, res) {
+    res.json(characters);
+});
+
+// Search for Specific Character (or all characters) - provides JSON
 app.get("/api/:characters?", function(req, res) {
     const chosen = req.params.characters;
 
@@ -50,23 +65,28 @@ app.get("/api/:characters?", function(req, res) {
             }
         }
 
-        return res.send("No character found");
+        return res.json(false);
     }
-
+    
     return res.json(characters);
 });
 
+// Create New Characters - takes in JSON input
 app.post("/api/new", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
     const newcharacter = req.body;
 
     console.log(newcharacter);
 
+    // We then add the json the user sent to the character array
     characters.push(newcharacter);
 
+    // We then display the JSON to the users
     res.json(newcharacter);
 });
 
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
