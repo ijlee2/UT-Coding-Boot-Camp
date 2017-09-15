2f1282d678972b68199c44100edb3fb9eac4332c
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-15 13:10:13 -0500

    day 35 exercises

---
+++ exercises/day35/calculator.js
@@ -0,0 +1,67 @@
+/****************************************************************************
+ ****************************************************************************
+    
+    Initialize
+    
+*****************************************************************************
+*****************************************************************************/
+const express    = require("express");
+const bodyParser = require("body-parser");
+
+const app  = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up Express to handle parsing data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": true}));
+app.use(bodyParser.text());
+
+
+
+/****************************************************************************
+ ****************************************************************************
+    
+    Set up routes
+    
+*****************************************************************************
+*****************************************************************************/
+// HTML routes
+app.get("/:operation/:number1/:number2", function(req, res) {
+    const x = parseFloat(req.params.number1);
+    const y = parseFloat(req.params.number2);
+    let output;
+
+    switch (req.params.operation) {
+        case "addition":
+            output = `${x} + ${y} = ${x + y}`;
+            break;
+
+        case "subtraction":
+            output = `${x} - ${y} = ${x - y}`;
+            break;
+
+        case "multiplication":
+            output = `${x} x ${y} = ${x * y}`;
+            break;
+
+        case "division":
+            output = `${x} / ${y} = ${(y !== 0) ? x / y : "undefined"}`;
+            break;
+
+    }
+
+    res.send(`<h1>${output}</h1>`);
+});
+
+
+
+/****************************************************************************
+ ****************************************************************************
+    
+    Helper functions
+    
+*****************************************************************************
+*****************************************************************************/
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
