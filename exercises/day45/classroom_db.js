2b5fe639a8e9a1d14599cee20e332dd7227165b9
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-29 14:05:56 -0500

    day 45 exercises

---
+++ exercises/day45/classroom_db.js
@@ -0,0 +1,44 @@
+use classroom_db;
+
+// Create students
+db.students.insert([
+    {
+        "name"     : "Isaac",
+        "rownumber": 5,
+        "os"       : "Win",
+        "hobbies"  : ["coding", "bouldering", "drinking coffee"]
+    },
+    {
+        "name"     : "David",
+        "rownumber": 4,
+        "os"       : "Mac",
+        "hobbies"  : ["coding", "playing basketball", "hanging out with friends"]
+    },
+    {
+        "name"     : "John",
+        "rownumber": 4,
+        "os"       : "Win",
+        "hobbies"  : ["coding", "practicing law", "walking out the dog"]
+    },
+    {
+        "name"     : "Jason",
+        "rownumber": 5,
+        "os"       : "Mac",
+        "hobbies"  : ["coding", "playing music", "drinking coffee"]
+    }
+]);
+
+// List every student
+db.students.find();
+
+// List one student
+db.students.find({"name": "Isaac"}).pretty();
+
+// List Mac-user students on row 5
+db.students.find({"rownumber": 5, "os": "Mac"}).pretty();
+
+// List students whose hobby includes drinking coffee
+db.students.find({"hobbies": "drinking coffee"});
+
+// Drop database
+db.dropDatabase();
