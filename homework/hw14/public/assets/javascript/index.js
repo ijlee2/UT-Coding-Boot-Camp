8021748e0c51e38048759161a0d98360d9b849f1
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-04 16:32:45 -0500

    stylesheet updated

--- homework/hw14/public/assets/javascript/index.js
+++ homework/hw14/public/assets/javascript/index.js
@@ -7,12 +7,17 @@ $(document).ready(function() {
     // Navbar for mobile
     $(".button-collapse").sideNav();
 
+    // Hide images by default
+    $("img").css({"display": "none"});
+
     $("#neotech-image").click(() => {
         console.log("index button clicked");
         // Toggle
         const displayImages = !($("#neotech-image i").text() === "image");
 
         $("img").css({"display": (displayImages) ? "block" : "none"});
+
+        $("#neotech-image i").attr({"title": (displayImages) ? "Click to hide images." : "Click to show images."});
         $("#neotech-image i").text((displayImages) ? "image" : "broken_image");
 
     });
--- homework/hw14/public/assets/javascript/thread.js
+++ homework/hw14/public/assets/javascript/thread.js
@@ -8,12 +8,17 @@ $(document).ready(function() {
     // Make the page responsive
     $("img").addClass("responsive-img");
 
+    // Hide images by default
+    $("img").css({"display": "none"});
+
     $("#neotech-image").click(() => {
         console.log("thread button clicked");
         // Toggle
         const displayImages = !($("#neotech-image i").text() === "image");
 
         $("img").css({"display": (displayImages) ? "block" : "none"});
+
+        $("#neotech-image i").attr({"title": (displayImages) ? "Click to hide images." : "Click to show images."});
         $("#neotech-image i").text((displayImages) ? "image" : "broken_image");
 
     });
