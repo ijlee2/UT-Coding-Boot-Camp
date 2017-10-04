dab3b9428179158b3873cf67c47cb248c2a8a2fb
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-04 11:29:07 -0500

    stylesheet updated

--- homework/hw14/public/assets/javascript/index.js
+++ homework/hw14/public/assets/javascript/index.js
@@ -1,7 +1,16 @@
+let displayImages = true;
+
 $(document).ready(function() {
     // Dropdown menu
     $(".dropdown-button").dropdown();
 
     // Navbar for mobile
     $(".button-collapse").sideNav();
+
+    $("#neotech-image").click(() => {
+        displayImages = !displayImages;
+
+        $("#neotech-image i").text((displayImages) ? "image" : "broken_image");
+
+    });
 });
