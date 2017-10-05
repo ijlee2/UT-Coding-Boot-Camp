5f13afc9cfd35370ae1bddb27eebe0211263d691
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-05 12:12:05 -0500

    day 49 exercises

--- exercises/day49/hello-react/src/App.js
+++ exercises/day49/hello-react/src/App.js
@@ -3,4 +3,10 @@ import HelloReact from "./components/HelloReact";
 
 const App = () => <HelloReact />;
 
+/* Babel translation
+var App = function() {
+    return React.createElement(HelloReact, null);
+}
+*/
+
 export default App;
--- exercises/day49/hello-react/src/components/HelloReact.js
+++ exercises/day49/hello-react/src/components/HelloReact.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 const HelloReact = () => <p>Hello World!</p>;
+//const HelloReact = function() { return <p>Hello World!</p>; }
 
 export default HelloReact;
