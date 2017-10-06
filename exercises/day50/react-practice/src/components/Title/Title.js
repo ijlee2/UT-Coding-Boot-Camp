cff8c712071f59fc4c3c3bfff5ba651e623e976e
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-06 12:00:00 -0500

    day 50 exercises

--- exercises/day50/react-practice/src/components/Title/Title.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Title.css";
-
-const Title = props => <h1 className="title">{props.children}</h1>;
-
-export default Title;
--- exercises/day50/react-practice/src/components/Wrapper/Wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Wrapper.css";
-
-const Wrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default Wrapper;
