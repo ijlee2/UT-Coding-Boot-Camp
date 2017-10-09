62bb095af2aa36475b537ef19fe266f3ca5682eb
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-09 14:01:07 -0500

    day 51 exercises

---
+++ exercises/day51/react-router/src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () =>
+  <div>
+    <h1>Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit,
+      lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi ac dui
+      fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras non risus
+      mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet ac. Sed nec
+      nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
+      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec
+      nec ornare elit. Donec blandit est sed risus feugiat porttitor. Vestibulum
+      molestie hendrerit massa non consequat. Vestibulum vitae lorem tortor. In
+      elementum ultricies tempus. Interdum et malesuada fames ac ante ipsum
+      primis in faucibus.
+    </p>
+  </div>;
+
+export default Home;
---
+++ exercises/day51/react-router/src/components/pages/Learn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Learn = () =>
+  <div>
+    <h1 className="text-center">hello</h1>
+    <p>
+      Lorem ipsum dolor sit amet, est ut enim consequat. Nostrum fastidii
+      partiendo sed ne, no mutat ludus aperiri mea, per in choro dolorem
+      electram. Invidunt reprimique assueverit quo ne, eruditi graecis pro ut.
+      Usu ut diceret scaevola evertitur, appareat voluptatibus ad vel.
+    </p>
+  </div>;
+
+export default Learn;
