c17e0d58fa0dbea3d943917062a74c25e447f5f9
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-10 13:48:13 -0500

    day 52 exercises

--- exercises/day52/ajax-books/client/src/App.js
+++ exercises/day52/ajax-books/client/src/App.js
@@ -14,7 +14,7 @@ const App = () =>
             <Switch>
                 <Route exact path="/" component={Books} />
                 <Route exact path="/books" component={Books} />
-                <Route path="/books/:id" component={Detail} />
+                <Route exact path="/books/:id" component={Detail} />
                 <Route component={NoMatch} />
             </Switch>
         </div>
