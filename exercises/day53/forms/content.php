6fad0b4b2bba28fcdc8e5ad105716fdcc2fd710e
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-11 13:29:31 -0500

    day 53 exercises

---
+++ exercises/day53/forms/content.php
@@ -0,0 +1,19 @@
+<form method="GET" action="">
+
+    <fieldset class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control" name="name" placeholder="Your Name">
+    </fieldset>
+
+    <fieldset class="form-group">
+        <label>Email</label>
+        <input type="text" class="form-control" name="email" placeholder="email@domain.com">
+    </fieldset>
+
+    <fieldset class="form-group">
+        <label>Age</label>
+        <input type="number" class="form-control" name="age" placeholder="Age">
+    </fieldset>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ exercises/day53/forms/index.php
@@ -0,0 +1,18 @@
+<!doctype>
+<html>
+    <head>
+        <title>Hello, User</title>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <?php print_r($_GET) ?>
+
+            <div class="jumbotron">
+                <h1>Welcome, Stranger!</h1>
+            </div>
+
+            <?php require './content.php' ?>
+        </div>
+    </body>
+</html>
